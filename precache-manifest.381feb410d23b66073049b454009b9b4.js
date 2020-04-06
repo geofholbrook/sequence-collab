56707989f978c7c6d993e8236293681a9e72b07a
Geoffrey Holbrook <geof@Geoffreys-MacBook-Pro.local>
2020-04-05 20:28:57 -0400

    Updates

---
+++ precache-manifest.381feb410d23b66073049b454009b9b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a511775d88d5608754248f640b00bffb",
+    "url": "/sequence-collab/index.html"
+  },
+  {
+    "revision": "8a9d895c29555b4b4e35",
+    "url": "/sequence-collab/static/css/main.d10aaceb.chunk.css"
+  },
+  {
+    "revision": "fd1a046c1a308e620667",
+    "url": "/sequence-collab/static/js/2.1876e775.chunk.js"
+  },
+  {
+    "revision": "2848759bb96b6b2b700b7bd477beceb1",
+    "url": "/sequence-collab/static/js/2.1876e775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a9d895c29555b4b4e35",
+    "url": "/sequence-collab/static/js/main.ec448a72.chunk.js"
+  },
+  {
+    "revision": "caef3a31d7c5a3da1a18",
+    "url": "/sequence-collab/static/js/runtime-main.06cbb84e.js"
+  }
+]);
