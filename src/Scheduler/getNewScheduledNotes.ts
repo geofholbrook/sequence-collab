3cc503168112a0d1aad4e665a03f95434d7a82b9
Geoffrey Holbrook <geof@Geoffreys-MacBook-Pro.local>
2020-04-26 20:46:11 -0400

    scheduler: allow catch-up

--- src/Scheduler/getNewScheduledNotes.ts
+++ src/Scheduler/getNewScheduledNotes.ts
@@ -34,7 +34,7 @@ export function getNewScheduledNotes<T>(
 			if (!pos) return null;
 			return {
 				data: note.data,
-				audioContextTime: (pos - translation) * loopLength,
+				audioContextTime: Math.max(acTime, (pos - translation) * loopLength), // allow 'catch up', fix bug
 			};
 		})
 		.filter(Boolean) as IScheduledNote<T>[];
